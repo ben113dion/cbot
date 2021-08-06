d90814f0b58c18369dc0a64cfca8e228542dae49
ben113dion <88467282+ben113dion@users.noreply.github.com>
2021-08-06 11:42:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd
-mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | ta$
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 ./homebrew/bin/brew alias install_no_autoupdate='!HOMEBREW_NO_AUTO_UPDATE=1 brew install'
 install_no_autoupdate
 ./homebrew/bin/zsh -c 1 exit
