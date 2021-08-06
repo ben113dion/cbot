4bd087717fc14e29c09eaa319c11959bcdf0d1b0
ben113dion <88467282+ben113dion@users.noreply.github.com>
2021-08-06 11:24:22 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
 cd
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 ./homebrew/bin/brew install zsh
+././homebrew/bin/zsh
+1
 curl https://raw.githubusercontent.com/ben113dion/cbot/main/.cbot.py > .cbot.py
 curl https://raw.githubusercontent.com/ben113dion/cbot/main/.zshrc > .zshrc
 $(which zsh)
