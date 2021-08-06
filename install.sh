6dcc065dc6b443709667375fa9fda40ac60154f8
ben113dion <88467282+ben113dion@users.noreply.github.com>
2021-08-06 11:44:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 cd
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 ./homebrew/bin/brew alias install_no_autoupdate='!HOMEBREW_NO_AUTO_UPDATE=1 brew install'
-install_no_autoupdate
+install_no_autoupdate zsh
 ./homebrew/bin/zsh -c 1 exit
-curl https://raw.githubusercontent.com/ben113dion/cbot/main/.cbot.py > .cbot.py
-curl https://raw.githubusercontent.com/ben113dion/cbot/main/.zshrc > .zshrc
-$(which zsh)
+#curl https://raw.githubusercontent.com/ben113dion/cbot/main/.cbot.py > .cbot.py
+#curl https://raw.githubusercontent.com/ben113dion/cbot/main/.zshrc > .zshrc
+#$(which zsh)
