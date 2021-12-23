b97810f389be93354db135a481cef64d54100eb5
ben113dion <88467282+ben113dion@users.noreply.github.com>
2021-12-23 12:12:10 -0500

    Update .cbot.py

--- .cbot.py
+++ .cbot.py
@@ -1,22 +1,6 @@
-from time import sleep
-import os
-import sys
-
-file = sys.argv[1];
-sleepTime = int(sys.argv[2]);
-
-
-
-def clean():
-	os.system("clear")
-	print("Running GCC Bot + Norminette\n\n\n");	
-
-def gcc():
-	os.system("gcc -Wall -Werror -Wextra "+file+";./a.out;")
-	print("\n\n\n\n");
-	os.system("norminette -R CheckForbiddenSourceHeader "+file+" | grep Error:");
-
+from time import sleep as s
+from os import system as c
+from sys import argv as a
 while True:
-	clean();
-	gcc();
-	sleep(sleepTime);
+	c("clear;"+str(a[1]))
+	s(3)
